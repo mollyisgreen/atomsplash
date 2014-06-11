808581bbc122dadcb6409f1ebc2472cc29320af3
Molly Yang <mollyisgreen@gmail.com>
2014-06-11 16:40:18 -0400

    clean up

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,3 @@
 exports.index = function(req, res){
   res.render('index.html', { title: 'Express' });
 };
-
-
-exports.autocomplete = function(req, res){
-  res.render('autocomplete.html', { title: 'Express' });
-};
